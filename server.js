250bed20b4c329a7a4982653459d6d888adf7b70
Rigoberto Sanchez <44029573+rigobertosanchez2@users.noreply.github.com>
2018-10-12 09:51:22 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+
+require('dotenv').config();
+
+var echo = require('laravel-echo-server');
+
+var options = {
+	"authHost": process.env.AUTH_HOST,
+	"authEndpoint": "/broadcasting/auth",
+	"clients": [
+		{
+			"appId": process.env.APP_ID,
+			"key": process.env.APP_KEY
+		}
+	],
+	"database": "redis",
+	"databaseConfig": {
+		"redis": {
+			"host": process.env.REDIS_HOST,  
+			"port": process.env.REDIS_PORT,
+			"password": process.env.REDIS_PASSWORD
+		}
+	},
+	"devMode": process.env.DEV_MODE,
+	"host": null,
+	"port": process.env.PORT,
+	"protocol": process.env.PROTOCOL,
+	"socketio": { 
+		"wsEngine": "ws"
+
+	},
+	"sslCertPath": "",
+	"sslKeyPath": "",
+	"sslCertChainPath": "",
+	"sslPassphrase": "",
+	"apiOriginAllow": {
+		"allowCors": false,
+		"allowOrigin": "",
+		"allowMethods": "",
+		"allowHeaders": ""
+	}
+}
+
+echo.run(options);
